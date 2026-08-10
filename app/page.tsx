56d1780c7c7cc600dad71a9d26390caadae44900
Velvet-Application <sh96hv64dj@privaterelay.appleid.com>
2026-08-10 20:55:47 +0200

    deploy: force Cloudflare frontend refresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FinanceShell from "./finance-shell";
 import MonetizationLayer from "./monetization-layer";
 
+// Cloudflare frontend deployment marker: finance-v1
 export default function Home() {
   return <MonetizationLayer><FinanceShell /></MonetizationLayer>;
 }
